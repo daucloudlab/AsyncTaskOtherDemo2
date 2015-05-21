a38c92f261faedda4d8928c609c1c4a24a1351bf
Daulet Baimbetov <daucloudlab@gmail.com>
2015-05-21 16:35:03 +0600

    version 02 Суретті желіден көшіріп қоя алады

--- app/src/main/java/kz/abcsoft/asynctaskotherdemo2/MainActivity.java
+++ app/src/main/java/kz/abcsoft/asynctaskotherdemo2/MainActivity.java
@@ -1,23 +1,42 @@
 package kz.abcsoft.asynctaskotherdemo2;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    ImageView logoImage ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new MyAsyncTask().execute();
+        logoImage = (ImageView) findViewById(R.id.ivFromSite);
+
+        String urlImage = "http://developer.alexanderklimov.ru/android/images/android_cat.jpg";
+        URL myUrl ;
+
+        try{
+            myUrl = new URL(urlImage) ;
+            new MyAsyncTask(logoImage).execute(myUrl) ;
+        } catch(MalformedURLException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -42,39 +61,47 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private class MyAsyncTask extends AsyncTask<Void, Integer, Void> {
+    private class MyAsyncTask extends AsyncTask<URL, Void, Bitmap> {
 
-        ProgressBar progress;
+        ImageView ivLogo;
+
+        public MyAsyncTask(ImageView iv) {
+            ivLogo = iv;
+        }
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            this.progress = (ProgressBar) findViewById(R.id.progress);
         }
 
         @Override
-        protected Void doInBackground(Void... voids) {
-            for (int i = 1; i <= 100; i++) {
-                SystemClock.sleep(100);
-                publishProgress(i);
+        protected Bitmap doInBackground(URL... urls) {
+            Bitmap networkBitmap = null;
+
+            URL networkUrl = urls[0];
+
+            try{
+                networkBitmap = BitmapFactory.decodeStream(networkUrl.openConnection().getInputStream()) ;
+            } catch (IOException e){
+                e.printStackTrace();
             }
 
-            return null;
+
+            return networkBitmap ;
         }
 
+//        @Override
+//        protected void onProgressUpdate(Integer... values) {
+//            super.onProgressUpdate(values);
+//
+//            this.progress.setProgress(values[0]);
+//        }
+
         @Override
-        protected void onProgressUpdate(Integer... values) {
-            super.onProgressUpdate(values);
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
 
-            this.progress.setProgress(values[0]);
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-
-            Toast.makeText(MainActivity.this, "Загрузка закончена!",
-                    Toast.LENGTH_LONG).show();
+            ivLogo.setImageBitmap(bitmap);
         }
     }
 }
